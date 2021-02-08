4cca86936df7790e7edcd29a820a32920a1744ab
shikhar <shikhar21121999@gmail.com>
2021-02-08 15:03:37 +0530

    commit from windows

---
+++ pthn_runserver.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "trying to run server"
+echo "currently at "
+ls
+cd /mnt/d/Projects/Web\ development/Django\ Based\ projects/xmeme_api/
+echo "we are at directory"
+ls
+python3 manage.py runserver
---
+++ variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+djproject='/mnt/d/Projects/Web\ development/Js\ based\ projects/'
+pwd
+echo $project
+cd $djproject
+pwd
